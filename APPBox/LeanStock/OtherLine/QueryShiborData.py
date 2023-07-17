4e599e1f4f81baeb239cebd19a7e8e8cae1e7e5d
a0xuejinlin <751979418@qq.com>
2023-07-17 18:24:14 +0800

    各类api接口2

--- APPBox/LeanStock/OtherLine/QueryShiborData.py
+++ APPBox/LeanStock/OtherLine/QueryShiborData.py
@@ -1,3 +1,22 @@
+# 参数含义：
+
+# start_date：开始日期，格式XXXX-XX-XX，发布日期在这个范围内，可以为空；
+# end_date：结束日期，格式XXXX-XX-XX，发布日期在这个范围内，可以为空。
+# 返回示例数据
+# date	shiborON	shibor1W	shibor2W	shibor1M	shibor3M	shibor6M	shibor9M	shibor1Y
+# 2015-01-04	3.640000	4.883000	5.794000	5.629000	5.138000	4.753700	4.624600	4.738500
+# 2015-01-05	3.421000	4.725000	5.665000	5.364000	5.125000	4.747800	4.627600	4.742900
+# 返回数据说明
+# 参数名称	参数描述
+# date	日期
+# shiborON	隔夜拆借利率
+# shibor1W	1周拆放利率
+# shibor2W	2周拆放利率
+# shibor1M	1个月拆放利率
+# shibor3M	3个月拆放利率
+# shibor6M	6个月拆放利率
+# shibor9M	9个月拆放利率
+# shibor1Y	1年拆放利率
 
 
 
